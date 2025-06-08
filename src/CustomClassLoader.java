5165d1a2468554e84322b1d0f292cba87dc81060
m11jr <alinurmammadzada@gmail.com>
2025-06-08 16:09:31 +0200

    Custom Class Loaders created

--- src/CustomClassLoader.java
+++ src/CustomClassLoader.java
@@ -8,13 +8,7 @@ public class CustomClassLoader extends ClassLoader {
     protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
         Class<?> loadedClass = findLoadedClass(name);
 
-//        if (loadedClass == null) {
-//            try {
-//                loadedClass = loadClass(name, false);
-//            } catch (ClassNotFoundException ex) {
-//                loadedClass = load(name);
-//            }
-//        }
+
         if (loadedClass != null) return loadedClass;
         if (!name.equals("Menu") && !name.equals("ScannerGet") && !name.equals("AdminLog") && !name.equals("UserLog") && !name.equals("DB")) {
             return super.loadClass(name, resolve);
@@ -48,29 +42,5 @@ public class CustomClassLoader extends ClassLoader {
         }
     }
 
-//     @Override
-//    public Class findClass(String name) throws ClassNotFoundException {
-//        byte[] classData = loadClassFromFile(name);
-//        return defineClass(name, classData, 0, classData.length);
-//    }
-
-//    private byte[] loadClassFromFile(String fileName) {
-//        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName+".class");
-//        byte[] classData;
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        int readValue=0;
-//        try {
-//            while ( (readValue = inputStream.read()) != -1 ) {
-//                byteArrayOutputStream.write(readValue);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        classData = byteArrayOutputStream.toByteArray();
-//        return classData;
-//
-//    }
-//
-//
 
 }
