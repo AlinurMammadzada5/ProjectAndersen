c221d2bbcb204bf7c02d33effcf9a3f9166fcbe8
m11jr <alinurmammadzada@gmail.com>
2025-06-17 00:00:58 +0200

    Streams,Lambda,Optional

--- src/DB.java
+++ src/DB.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
+
 
 
 public class DB {
--- src/UserLog.java
+++ src/UserLog.java
@@ -1,4 +1,4 @@
-import java.util.Optional;
+
 
 public class UserLog {
     String username;
